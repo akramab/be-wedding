35ed670cea4016c1a3c524fccfb0dc8da9ceab0b
akramab <13519142@std.stei.itb.ac.id>
2023-06-30 14:41:52 +0700

    feat: add cors option enable

--- internal/rest/server.go
+++ internal/rest/server.go
@@ -28,7 +28,12 @@ func New(
 
 	r.Use(
 		cors.Handler(cors.Options{
-			AllowedOrigins: []string{"https://*", "http://*"},
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: true,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
 		}),
 		middleware.HTTPTracer,
 		middleware.RequestID(zlogger),
