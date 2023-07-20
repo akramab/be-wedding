264daccf7aa63141e0c4d34af1f2f0cfa4e038a5
akramab <13519142@std.stei.itb.ac.id>
2023-07-20 19:29:21 +0700

    feat: add wa message to qr mobile handler

--- internal/rest/handler/user/validate_user_qr_rsvp.go
+++ internal/rest/handler/user/validate_user_qr_rsvp.go
@@ -2,9 +2,14 @@ package user
 
 import (
 	"be-wedding/internal/rest/response"
+	"context"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"google.golang.org/protobuf/proto"
 )
 
 type ValidateUserQRRsvpResponse struct {
@@ -13,36 +18,73 @@ type ValidateUserQRRsvpResponse struct {
 	VIP         bool   `json:"vip"`
 }
 
+const (
+	GetCurrentAdmin1 = "CURRENT_ADMIN_1"
+	GetCurrentAdmin2 = "CURRENT_ADMIN_2"
+
+	StringSeparator = ","
+)
+
 func (handler *userHandler) ValidateUserQRRsvp(w http.ResponseWriter, r *http.Request) {
-	// ctx := r.Context()
+	ctx := r.Context()
 	invitationCode := chi.URLParam(r, "invitation_code")
+	atCode := r.URL.Query().Get("code")
+
+	var adminList []string
+	var resp ValidateUserQRRsvpResponse
+	if atCode == "1" {
+		adminListString, _ := handler.redisCache.Get(ctx, GetCurrentAdmin1).Result()
+		if adminListString == "" {
+			adminListString = strings.Join([]string{"+62812155249"}, StringSeparator) // random number
+		}
+
+		adminList = strings.Split(adminListString, ",")
+	} else {
+		adminListString, _ := handler.redisCache.Get(ctx, GetCurrentAdmin2).Result()
+		if adminListString == "" {
+			adminListString = strings.Join([]string{"+62812155249"}, StringSeparator) // random number
+		}
+
+		adminList = strings.Split(adminListString, ",")
+	}
 
 	if invitationCode == "0c467423-e324-4786-a9d9-0c77eb267407" {
-		resp := ValidateUserQRRsvpResponse{
+		resp = ValidateUserQRRsvpResponse{
 			Name:        "Bapak Ari",
 			PeopleCount: 2,
 			VIP:         true,
 		}
-
-		response.Respond(w, http.StatusOK, resp)
-		return
 	}
 
 	if invitationCode == "69fee15d-2c45-48ea-982e-4ce6327298fc" {
-		resp := ValidateUserQRRsvpResponse{
+		resp = ValidateUserQRRsvpResponse{
 			Name:        "Bapak Lukman",
 			PeopleCount: 10,
 			VIP:         false,
 		}
-
-		response.Respond(w, http.StatusOK, resp)
-		return
+	} else {
+		resp = ValidateUserQRRsvpResponse{
+			Name:        "Tamu Tidak Diundang",
+			PeopleCount: -99999,
+			VIP:         false,
+		}
 	}
 
-	resp := ValidateUserQRRsvpResponse{
-		Name:        "Tamu Tidak Diundang",
-		PeopleCount: -99999,
-		VIP:         false,
+	textForAdmin := `Konfirmasi Kehadiran Berhasil!
+
+*Berikut data tamu undangan*
+
+Nama: %s
+Jumlah Konfirmasi (orang): 	%d
+VIP: %s`
+	for _, admin := range adminList {
+		handler.waClient.SendMessage(context.Background(), admin, &waProto.Message{
+			Conversation: proto.String(fmt.Sprintf(textForAdmin,
+				resp.Name,
+				resp.PeopleCount,
+				resp.VIP,
+			)),
+		})
 	}
 
 	response.Respond(w, http.StatusOK, resp)
